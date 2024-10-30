84825ed3f1a8122fc3cf884bc912d742c6458218
Pietro Rocchio <32908126+pietro2356@users.noreply.github.com>
2024-10-30 09:20:28 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import {add} from "@math/add";
+
+console.log("Somma 3 e 6: ", add(3, 6)); // 9@ma
---
+++ src/math/add.ts
@@ -0,0 +1,4 @@
+const add = (a: number, b: number): number => a + b;
+const subtract = (a: number, b: number): number => a - b;
+
+export {add, subtract};
